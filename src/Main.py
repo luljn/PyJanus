ee20c0e321c48ccd2cab165a186a62f33fe4dc62
luljn <mbohlulajonathan4@gmail.com>
2026-04-26 21:38:39 +0200

    Code refactored

--- src/Main.py
+++ src/Main.py
@@ -1,13 +1,31 @@
 # MAIN CLASS
 
+from kernel.Kernel import Kernel
+
+from os import system 
+from sys import platform
+
+"""The main class : its role is to start the Kernel.
+"""
 class Main : 
     
     @staticmethod
-    def main() -> None :
-        print("PyJanus is working :) !") 
+    def run() -> None :
+        
+        # OS recognition for clearing screen.
+        match platform : 
+            case "win32" :    
+                system("cls")
+            case _ :    
+                system("clear")
+        
+        kernel:Kernel = Kernel.getInstance()
+        print("\nPyJanus is working :) !")
+        kernel.start()
+        kernel.stop()
 
 
 
 if __name__ == '__main__' :
     
-    Main.main()
+    Main.run()
