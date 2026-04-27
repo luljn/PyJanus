56e3a417f36c4f4c017e6cddd1726d96faafcec5
luljn <mbohlulajonathan4@gmail.com>
2026-04-27 23:51:27 +0200

    Event added

---
+++ src/event/Event.py
@@ -0,0 +1,28 @@
+###
+
+class Event() :
+    
+    #
+    def __init__(self) :
+        
+        pass
+    
+    #
+    def getSource()-> None :
+        
+        pass
+    
+    #
+    def setSource()-> None :
+        
+        pass
+    
+    #
+    def getSource()-> None :
+        
+        pass
+    
+    # toString equivalent in Python.
+    def __str__(self)-> None :
+        
+        return 'Event\n'
