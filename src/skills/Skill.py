578cbe213440c5f0737b991b0d42099d4d918406
luljn <mbohlulajonathan4@gmail.com>
2026-04-27 23:53:44 +0200

    Skill base implementation

---
+++ src/skills/Skill.py
@@ -0,0 +1,14 @@
+###
+
+from abc import ABC, abstractmethod
+
+from capacities.Capacity import Capacity
+
+class Skill(ABC, Capacity) :
+    
+    referenceCount: int = 0
+    
+    @abstractmethod
+    def __init__(self) :
+        
+        Skill.referenceCount+= 1
