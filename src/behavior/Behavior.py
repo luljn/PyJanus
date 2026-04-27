cc6e843c04e9861b353a0df94873fd64393a2ec9
luljn <mbohlulajonathan4@gmail.com>
2026-04-27 23:52:59 +0200

    Behavior base implementation

---
+++ src/behavior/Behavior.py
@@ -0,0 +1,30 @@
+####
+
+from agent.Agent import Agent
+
+class Behavior() :
+    
+    #
+    def __init__(self) :
+        
+        pass
+    
+    #
+    def onStart()-> None :
+        
+        pass
+    
+    #
+    def onStop()-> None :
+        
+        pass
+    
+    #
+    def getOwner()-> Agent :
+        
+        pass
+    
+    #
+    def asEventListener()-> None :
+        
+        pass
