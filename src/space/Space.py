bf9c9e113412c0d7ff5d2e9b9f695a23776d4948
luljn <mbohlulajonathan4@gmail.com>
2026-05-31 23:18:46 +0200

    Space upgraded

--- src/space/Space.py
+++ src/space/Space.py
@@ -1,20 +1,33 @@
 # class Space
 
-from uuid import UUID
+from uuid import UUID, uuid4
 
 class Space :
     
-    #
-    def __init__(self) :
+    # Constructor
+    def __init__(self, name: str = None) :
         
+        self.__id: UUID = uuid4()
+        self.__name = name if name is not None else f'Default_Space_{self.__id}'
         self.__participants: list[UUID] = []
     
-    #
+    # Get all the participants of the space
     def getParticipants(self)-> list[UUID] :
         
         return self.__participants
     
-    #
+    # Add a participant to the space.
     def addParticipant(self, agentId: UUID)-> None : 
         
         self.__participants.append(agentId)
+    
+    # To emit an event in the space.
+    def emit() :
+        pass
+    
+    # Getters
+    def getID(self)-> UUID :
+        return self.__id
+    
+    def getName(self)-> UUID :
+        return self.__name
