2195b9990eddb77555ed83376a21204084699fbc
luljn <mbohlulajonathan4@gmail.com>
2026-05-31 23:30:07 +0200

    Skills base implementation

--- src/skills/EventSkill.py
+++ src/skills/EventSkill.py
@@ -1,7 +1,12 @@
 # Event Skill
 
 from .Skill import Skill
+from agent.Agent import Agent
 from capacities.EventCapacity import EventCapacity
+from kernel.Kernel import Kernel
+from services.EventService import EventService
+from services.DirectoryService import DirectoryService
+from space.Space import Space
 
 class EventSkill(Skill, EventCapacity) :
     
@@ -11,16 +16,22 @@ class EventSkill(Skill, EventCapacity) :
         super().__init__()
     
     #
-    def emit()-> None :
+    def emit(self)-> None :
         
         pass
     
     #
-    def receive()-> None : 
+    def receive(self)-> None : 
         
         pass
     
     #
-    def wake()-> None : 
+    def wake(self)-> None : 
         
         pass
+    
+    # To register an agent in a space.
+    def registerInSpace(self, agent: Agent, space: Space)-> None :
+        
+        Kernel.getInstance().getService(EventService).registerAgent(agent, space)
+        Kernel.getInstance().getService(DirectoryService).register_agent(agent)
--- src/skills/SpawnSkill.py
+++ src/skills/SpawnSkill.py
@@ -1,6 +1,7 @@
 # Spawn Skill
 
 from .Skill import Skill
+from agent.Agent import Agent
 from capacities.LifeCycleCapacity import LifeCycleCapacity
 
 class SpawnSkill(Skill, LifeCycleCapacity) :
@@ -9,3 +10,23 @@ class SpawnSkill(Skill, LifeCycleCapacity) :
     def __init__(self) :
         
         super().__init__()
+    
+        """
+        Creates a new agent instance of a given type.
+    """
+    def spawn(self, user: Agent, agentToSpawned: Agent) -> None :
+        
+        pass
+    
+    """
+        Demands the arrest and destruction of the current agent.
+    """
+    def killme(self, user: Agent) -> None :
+        
+        pass
+    
+    """ 
+        Creates an agent in a specific context.
+    """
+    def spawn_in_context(self, user: Agent, agent: Agent, context_id, *args) -> None :
+        pass 
