d75274764b6f7743e0cb8506029e48029dd18950
luljn <mbohlulajonathan4@gmail.com>
2026-05-31 23:28:25 +0200

    Capacities base implementation

--- src/capacities/LifeCycleCapacity.py
+++ src/capacities/LifeCycleCapacity.py
@@ -1,23 +1,37 @@
 # LifeCycle Capacity
 
-from abc import ABC
+from abc import ABC, abstractmethod
 
 from agent.Agent import Agent
 from .Capacity import Capacity
 
-class LifeCycleCapacity(ABC, Capacity) :
+"""
+    Capacity defining the actions related to the agent's lifecycle.
+"""
+class LifeCycleCapacity(Capacity) :
     
-    #
-    def __init__(self) :
-        
-        super().__init__()
+    """ def __init__(self, owner=None):
+        super().__init__(owner) """
     
-    #
-    def spawn(a: Agent)-> None :
+    """
+        Creates a new agent instance of a given type.
+    """
+    @abstractmethod
+    def spawn(self, user: Agent, agent: Agent) -> None :
         
         pass
     
-    #
-    def killMe()-> None : 
+    """
+        Demands the arrest and destruction of the current agent.
+    """
+    @abstractmethod
+    def killme(self, user: Agent) -> None :
         
         pass
+    
+    """ 
+        Creates an agent in a specific context.
+    """
+    @abstractmethod
+    def spawn_in_context(self, user: Agent, agent: Agent, context_id, *args) -> None :
+        pass 
