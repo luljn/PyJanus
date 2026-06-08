45873215b062e2134aafa2ea2c140d40e2e93652
luljn <mbohlulajonathan4@gmail.com>
2026-06-08 20:22:40 +0200

    Data type of participant in a space changed from UUID to Agent

--- src/space/Space.py
+++ src/space/Space.py
@@ -1,6 +1,9 @@
 # class Space
 
 from uuid import UUID, uuid4
+from agent.Agent import Agent
+
+from event.Event import Event
 
 class Space :
     
@@ -8,26 +11,29 @@ class Space :
     def __init__(self, name: str = None) :
         
         self.__id: UUID = uuid4()
-        self.__name = name if name is not None else f'Default_Space_{self.__id}'
-        self.__participants: list[UUID] = []
+        self.__name = name if name is not None else f'Space_{self.__id}'
+        self.__participants: list[Agent] = []
     
     # Get all the participants of the space
-    def getParticipants(self)-> list[UUID] :
+    def getParticipants(self)-> list[Agent] :
         
         return self.__participants
     
     # Add a participant to the space.
-    def addParticipant(self, agentId: UUID)-> None : 
+    def addParticipant(self, agent: Agent)-> None : 
         
-        self.__participants.append(agentId)
+        self.__participants.append(agent)
     
     # To emit an event in the space.
-    def emit() :
-        pass
+    def emit(self, event: Event)-> None :
+        
+        for participant in self.getParticipants() :
+            
+            participant._receive()
     
     # Getters
     def getID(self)-> UUID :
         return self.__id
     
-    def getName(self)-> UUID :
+    def getName(self)-> str :
         return self.__name
