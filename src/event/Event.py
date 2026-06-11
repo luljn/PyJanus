efbf5d02ff302798d62fbeb6fe16f4cc665aa90e
luljn <mbohlulajonathan4@gmail.com>
2026-06-11 09:57:17 +0200

    getters and setters added

--- src/event/Event.py
+++ src/event/Event.py
@@ -16,20 +16,30 @@ class Event :
         self.data = data
         self.timestamp = timestamp
     
+    # To get the ID.
+    def getID(self)->UUID : return self.id
+    
     # To get the source of the event.
     def getSource(self)-> Any :
         
         return self.source
+    
+    # To get the data of the event.
+    def getData(self)-> Any :
+        
+        return self.data
     
     # To set the source of the event.
     def setSource(self, source_:Any)-> None :
         
         self.source = source_
     
+    # To set the data of the event.
+    def setData(self, data_:Any)-> None :
+        
+        self.data = data_
+    
     # toString equivalent in Python.
     def __str__(self)-> None :
         
-        return f'Event_{self.id} trigerred\ndata : {self.data}\n'
-    
-    # To get the ID.
-    def getID(self)->UUID : return self.id
+        return f'Event_{self.getID()} trigerred\ndata : {self.getData()}\n'
